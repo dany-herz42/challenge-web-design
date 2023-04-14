e1c72caef7775e052d97d288982efb2d2d870f05
dany_herz <danyherzdev@gmail.com>
2023-04-14 13:37:20 -0600

    refactor(components): refactor code

--- src/components/CardPerson.jsx
+++ src/components/CardPerson.jsx
@@ -1,43 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import ColtonPhoto from '../assets/image-colton.jpg';
-import IrenePhoto from '../assets/image-irene.jpg';
-import AnnePhoto from '../assets/image-anne.jpg';
+import React, { useState, useEffect } from "react";
+import { Row, Col, Image } from "react-bootstrap";
+import ColtonPhoto from "../assets/image-colton.jpg";
+import IrenePhoto from "../assets/image-irene.jpg";
+import AnnePhoto from "../assets/image-anne.jpg";
 
-const CardPerson = (props) => {
-  const [photo, setphoto] = useState(null)
+const CardPerson = ({ photo: profilePhoto, name, text, align }) => {
+  const [photo, setPhoto] = useState(null);
+
   useEffect(() => {
-    switch (props.photo) {
-      case "colton":
-        setphoto(ColtonPhoto)
-        break;
-      case "irene":
-        setphoto(IrenePhoto)
-        break;
-      case "anne":
-        setphoto(AnnePhoto)
-        break;
-      default:
-        break;
-    }
-  }, [props.photo])
+    const photos = {
+      colton: ColtonPhoto,
+      irene: IrenePhoto,
+      anne: AnnePhoto,
+    };
+    setPhoto(photos[profilePhoto]);
+  }, [profilePhoto]);
+
   return (
-    <div className={`card-people__container me-2 py-4 px-4 ${props.align}`}>
+    <div className={`card-people__container me-2 py-4 px-4 ${align}`}>
       <Row>
         <Col xs={2} sm={2} lg={2}>
           <Image src={photo} roundedCircle width="40" />
         </Col>
         <Col>
-          <p className="card-people__name">{props.name} <br /> <span>Verified Buyer</span></p>
+          <p className="card-people__name">
+            {name} <br /> <span>Verified Buyer</span>
+          </p>
         </Col>
       </Row>
       <Row>
         <Col>
-          <p className="card-people__text">"{props.text}"</p>
+          <p className="card-people__text">"{text}"</p>
         </Col>
       </Row>
     </div>
-  )
-}
+  );
+};
 
-export default CardPerson
+export default CardPerson;
--- src/components/CardStarsComp.jsx
+++ src/components/CardStarsComp.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import star from '../assets/icon-star.svg';
-import { Row, Col } from 'react-bootstrap';
+import React from "react";
+import star from "../assets/icon-star.svg";
+import { Row, Col } from "react-bootstrap";
 
 const CardStarsComp = (props) => {
   return (
     <Row className={`justify-content-xs-center ${props.justify}`}>
-      <Col xs={12} sm={12} md={12} lg={6} xl={11} className="pt-2 my-2 card-stars">
+      <Col
+        xs={12}
+        sm={12}
+        md={12}
+        lg={6}
+        xl={11}
+        className="pt-2 my-2 card-stars"
+      >
         <Row className="justify-content-center">
-          <Col xs ={5} sm={4} md={6} lg={12} xl={4}>
-            <div style={{display: "flex", justifyContent: "space-between "}}>
-              <img className="px-2" src={star} />
-              <img className="px-2" src={star} />
-              <img className="px-2" src={star} />
-              <img className="px-2" src={star} />
-              <img className="px-2" src={star} />
+          <Col xs={5} sm={4} md={6} lg={12} xl={4}>
+            <div style={{ display: "flex", justifyContent: "space-between " }}>
+              {[1, 2, 3, 4, 5].map((item) => (
+                <img className="px-2" alt="start" src={star} key={item} />
+              ))}
             </div>
           </Col>
           <Col sm={12} md={12} lg={8}>
@@ -22,7 +27,7 @@ const CardStarsComp = (props) => {
         </Row>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default CardStarsComp
+export default CardStarsComp;
